98a8ff800ad8caf39d435872883baddd15207db8
Ibraim Abduramanov <freerancho@gmail.com>
2020-12-12 23:44:50 +0300

    28 task, Python

---
+++ Practice/28/Python/28.py
@@ -0,0 +1,26 @@
+def print_factorization(n: int) -> None:
+   ans = [] 
+   i = 2
+   while (i * i < n): 
+      if (n % i == 0): 
+         ans += [[i, 0]]; 
+         while n % i == 0: 
+            n //= i 
+            ans[len(ans)-1][1] += 1 
+      i += 1
+   if n > 1:
+      ans += [[n, 1]] 
+      
+   sorted(ans, key=lambda pair: pair[0])
+   
+   i = 0
+   for divider, count in ans:
+      spec_char = '' if( i == 0 )else '*'; i += 1
+      if count > 1:
+         print(f'{spec_char}{divider}^{count}', end='')
+      else:
+         print(f'{spec_char}{divider}', end='')
+
+
+n = int(input("Введите число: ")) 
+print_factorization(n)
